9dee72cfc9d96c4a4c27bf1f49dbc9705e7316a2
Victor Filatov <victorfil001@gmail.com>
2017-05-19 22:02:19 -0400

    nonworking

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,5 +1,5 @@
 for (var i = 1; i < 101; i = i + 1) {
-   if ( i % 3 == 0)
+   if ( i % 3 == 0 &&  (! (i % 5 == 0)))
   console.log('Fizz');
   if ( i % 3 == 0 && i % 5 == 0)
   console.log('FizzBuzz');
