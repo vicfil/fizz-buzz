3172767ba0617878eee3b5c9637d0c9429f9d5ac
Victor Filatov <victorfil001@gmail.com>
2017-05-19 21:55:19 -0400

    final version with FizzBuzz

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -2,9 +2,9 @@ for (var i = 1; i < 101; i = i + 1) {
    if ( i % 3 == 0)
   console.log('Fizz');
   if ( i % 3 == 0 && i % 5 == 0)
-  console.log('Fizz');
+  console.log('FizzBuzz');
   else if ( i % 5 == 0)
   console.log('Buzz');
   else
-  console.log(i); 
+  console.log(i);
 }
