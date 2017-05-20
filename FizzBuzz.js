62761c87741808c44e76f5ee5fd1828950bc0cb0
Victor Filatov <victorfil001@gmail.com>
2017-05-19 21:42:54 -0400

    works perfectly

--- FizzBuzz.js
+++ FizzBuzz.js
@@ -1,10 +1,10 @@
 for (var i = 1; i < 101; i = i + 1) {
-  /* if ( i % 3 == 0)
-  console.log('Fizz'); */
+   if ( i % 3 == 0)
+  console.log('Fizz');
   if ( i % 3 == 0 && i % 5 == 0)
-  console.log('Fizz1');
-  /* else if ( i % 5 == 0)
+  console.log('Fizz');
+  else if ( i % 5 == 0)
   console.log('Buzz');
   else
-  console.log(i); */
+  console.log(i); 
 }
